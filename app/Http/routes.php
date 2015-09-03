d6d5656feb665f122c14b36bdc34fe96a4e38bc5
Eray Aydın <eray@erayaydin.me>
2015-09-04 00:05:06 +0300

    Added basic login routes and fixed auth middleware paths

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+Route::group(["middleware" => "guest"], function(){
+    Route::get("login", ["as" => "user.login", "uses" => "UserController@login"]);
+    Route::post("login", ["as" => "user.login", "uses" => "UserController@doLogin"]);
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(["middleware" => "auth"], function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
